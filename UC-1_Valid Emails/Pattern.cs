0590b6e2fc1c58f1b1817d01d70011b6ac8ac2f3
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 18:48:01 +0530

    Check email validation for UC-1

---
+++ UC-1_Valid Emails/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_1_Valid_Emails
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = @"^([a-z]+)?@([a-z]+)\.([a-z\,]{2,})$";
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, REGEX_CODE);
+        }
+    }
+}
---
+++ UC-1_Valid Emails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_1_Valid_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter Email Id: ");
+            string email = Console.ReadLine();
+            bool id = pat.Validate_Email(email);
+            Console.WriteLine(id);
+            if (id)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
