43863aa934173af975e9d5d70a9e8199d66c6d1d
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:08:33 +0530

    Check email validation for UC-8

---
+++ UC-8_Valid_Email/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_8_Valid_Email
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = @"^([a-z]+)?@([a-z]+)\.([a-z]{2,5})(\.[a-z]{3})?$";
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, REGEX_CODE);
+        }
+    }
+}
