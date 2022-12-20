4cef3be2fff88e327b375ffa563ebd60c10cd2e4
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:00:17 +0530

    Check email validation for UC-4

---
+++ UC-4_Valid_Email/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_4_Valid_Email
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = @"^([a-z0-9]+)?@([a-z]+)\.([a-z\,]{2,})$";
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, REGEX_CODE);
+        }
+    }
+}
