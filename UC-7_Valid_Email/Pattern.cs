4c17a30c2d7fd1caa6d2e607a56b685232421a7c
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:06:58 +0530

    Check email validation for UC-7

---
+++ UC-7_Valid_Email/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_7_Valid_Email
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = @"^([a-z]+)?@([1]+)\.([a-z\,]{2,})$";
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, REGEX_CODE);
+        }
+    }
+}
