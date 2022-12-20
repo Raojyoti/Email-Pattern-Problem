ba4013aaaa778860dfbdbb3fca45a449dbb97f41
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:10:15 +0530

    Check email validation for UC-9

---
+++ UC-9_Valid_Email/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_9_Valid_Email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter Email Id: ");
+            string email = Console.ReadLine();
+            bool id = pat.Validate_Email(email);
+            Console.WriteLine(id);
+            if (id)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
